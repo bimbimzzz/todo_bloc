491ecc077ad05d3d790bce8a527ab83c7b0b9cfd
Bimbimzzze <kresno.bimantoro@gmail.comm>
2023-06-22 22:49:45 +0700

    first commit

---
+++ lib/blocs/active_todo_count/active_todo_count_state.dart
@@ -0,0 +1,28 @@
+part of 'active_todo_count_bloc.dart';
+
+class ActiveTodoCountState extends Equatable {
+  const ActiveTodoCountState({
+    required this.activeTodoCount,
+  });
+
+  factory ActiveTodoCountState.initial() {
+    return const ActiveTodoCountState(activeTodoCount: 0);
+  }
+
+  final int activeTodoCount;
+
+  @override
+  List<Object> get props => [activeTodoCount];
+
+  @override
+  String toString() =>
+      'ActiveTodoCountState(activeTodoCount: $activeTodoCount)';
+
+  ActiveTodoCountState copyWith({
+    int? activeTodoCount,
+  }) {
+    return ActiveTodoCountState(
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
+    );
+  }
+}
